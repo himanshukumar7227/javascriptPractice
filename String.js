ec6c7b0f6f849a9ae5076c50add38876b1cdfe4c
himanshukumar7227 <himanshukumar72270191@gamil.com>
2025-01-25 19:09:09 +0530

    java samples

---
+++ String.js
@@ -0,0 +1,19 @@
+let name ="Himanshu";
+console.log(name.length);
+sentance=`hello friend`;
+console.log(sentance);
+let gm=name.toUpperCase();
+console.log(gm);
+let gm2=name.slice(2,4);
+console.log(gm2);
+let gm3=name.replace('Hi',' ');
+console.log(gm3);
+let gm4=name.concat(" Hello");
+console.log(gm4);
+let gm5="   hi  ";
+console.log(gm5);
+console.log(gm5.trim());
+for(let i=0;i<name.length;i++){
+    console.log(name[i]);
+}
+
---
+++ functions.js
@@ -0,0 +1,7 @@
+function sum_of_Two_no(){
+    let a =1;
+    let b=2;
+    let c=a+b;
+    console.log(c);
+}
+sum_of_Two_no();
